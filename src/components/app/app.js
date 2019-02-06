fb88ac57e44d802a524c8d0b70c78d8c95be8394
Vira Melnyk <vira.melnyk@vordik.com>
2019-02-06 10:58:55 +0100

    Components refactoring

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,22 +1,21 @@
+
 import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorButton from '../error-button';
-import ErrorIndicator from '../error-indicator';
 import PeoplePage from '../people-page';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import ErrorBoundry from "../error-boundry";
+import Row from "../row/row";
+import ItemDetails from "../item-details/item-details";
 import SwapiService from '../../services/swapi-service';
 
 import './app.css';
 
 export default class App extends Component {
-
   swapiService = new SwapiService();
   state = {
-    showRandomPlanet: true,
-    hasError: false
+    showRandomPlanet: true
   };
 
   toggleRandomPlanet = () => {
@@ -27,61 +26,27 @@ export default class App extends Component {
     });
   };
 
-  componentDidCatch() {
-    this.setState({ hasError: true });
-  };
-
   render() {
 
-    if (this.state.hasError) {
-      return <ErrorIndicator />
-    }
-
-    const planet = this.state.showRandomPlanet ?
-      <RandomPlanet/> :
-      null;
+    const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null;
+    const {getPerson, getStarship, getPersonImage, getStarshipImage} = this.swapiService;
+    const personDetails = (
+      <ItemDetails itemId={11} getData={getPerson} getImageUrl={getPersonImage} />
+      );
+    const starshipDetails = (
+      <ItemDetails itemId={5} getData={getStarship} getImageUrl={getStarshipImage} />
+      );
 
     return (
-      <div className="stardb-app">
-        <Header />
-        { planet }
-
-        <div className="row mb2 button-row">
-          <button
-            className="toggle-planet btn btn-warning btn-lg"
-            onClick={this.toggleRandomPlanet}>
-            Toggle Random Planet
-          </button>
-          <ErrorButton />
+      <ErrorBoundry>
+        <div className="stardb-app">
+          <Header />
+          
+          <Row
+              left={personDetails}
+              right={starshipDetails} />
         </div>
-
-        <PeoplePage getData={this.swapiService.getAllPeople} />
-
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList 
-              onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllPlanets}
-              renderItem={(item) => (<span>{item.name} <button>!</button></span>)}
-             />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList 
-              onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllStarships}
-              renderItem={(item) => item.name}/>
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
-
-      </div>
+      </ErrorBoundry>
     );
   }
 }
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './item-list.css';
+import SwapiService from "../../services/swapi-service";
 import Spinner from "../spinner/spinner";
 
 export default class ItemList extends Component {
@@ -23,17 +24,17 @@ export default class ItemList extends Component {
 
   renderItems(arr) {
     return arr.map((item) => {
-      const {id} = item;
-      const label = this.props.renderItem(item);
-    
+      const { id } = item;
+      const label = this.props.children(item);
+
       return (
         <li className="list-group-item"
-          key={id}
-          onClick={() => this.props.onItemSelected(id)}>
+            key={id}
+            onClick={() => this.props.onItemSelected(id)}>
           {label}
         </li>
-       )
-    })
+      );
+    });
   }
 
   render() {
